4a833f9744f93c2b947f227952a4d0dbf1db3c99
Wesleyhdias <103648122+Wesleyhdias@users.noreply.github.com>
2022-04-14 13:38:46 -0300

    Update Flappy bird.py

--- Flappy bird.py
+++ Flappy bird.py
@@ -26,7 +26,7 @@ for c in range(2):
 
 for c in range(3):
     ys = obj.pipeys()
-    X = c*225 + 400
+    x = c*225 + 400
     pipe = obj.Pipe(os.path.join(img_dir, 'pipe.png'), x, ys[0], 4)
     pipe1 = obj.Pipe(os.path.join(img_dir, 'pipe.png'), x, ys[1], 4, True)
     all_sprites.add(pipe)
