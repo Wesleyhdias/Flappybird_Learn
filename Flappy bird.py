d2350f91e8d43374488a3ae4aa6e5b4367b7329c
Wesleyhdias <103648122+Wesleyhdias@users.noreply.github.com>
2022-06-25 16:19:02 -0300

    Update Flappy bird.py

--- Flappy bird.py
+++ Flappy bird.py
@@ -2,58 +2,77 @@ import pygame as py
 import obj
 import os
 from random import randint
-from pygame.locals import *
+from pygame.locals import * 
 from sys import exit
 
 py.init()
 
-color = {'Blue': (0, 0, 255), 'Red': (255, 0, 0), 'Green': (0, 255, 0),
-         'White': (255, 255, 255), 'Gray': (100, 100, 100)}
-defalt_dir = os.path.dirname(__file__)
-img_dir = os.path.join(defalt_dir, "img")
-bird_sheet = os.path.join(img_dir, 'bird.png')
-largura, altura = 500, 800
-tela = py.display.set_mode((largura, altura))
-py.display.set_caption('Flappy Bird')
-fps = py.time.Clock()
-
-all_sprites = py.sprite.Group()
-
-for c in range(2):
-    bg = obj.Bg(os.path.join(img_dir, 'bg.png'), c, 1000, 1)
-    all_sprites.add(bg)
-
-for c in range(3):
-    ys = obj.pipeys()
-    x = c*225 + 400
-    pipe = obj.Pipe(os.path.join(img_dir, 'pipe.png'), x, ys[0], 4)
-    pipe1 = obj.Pipe(os.path.join(img_dir, 'pipe.png'), x, ys[1], 4, True)
-    all_sprites.add(pipe)
-    all_sprites.add(pipe1)
-
-for c in range(2):
-    piso = obj.Bg(os.path.join(img_dir, 'base.png'), c, 150, 4)
-    all_sprites.add(piso)
-
-bird = obj.Bird(bird_sheet)
-all_sprites.add(bird)
-
 while True:
-    
-    tela.fill(color['Gray'])
-    fps.tick(60)
-    
-    for event in py.event.get():
-        if event.type == QUIT:
-            py.quit()
-            exit()
+    color = {'Blue': (0, 0, 255), 'Red': (255, 0, 0), 'Green': (0, 255, 0),
+            'White': (255, 255, 255), 'Gray': (100, 100, 100)}
+    play = True
+    start = False
+    defalt_dir = os.path.dirname(__file__)
+    img_dir = os.path.join(defalt_dir, "img")
+    bird_sheet = os.path.join(img_dir, 'bird.png')
+    largura, altura = 500, 800
+    tela = py.display.set_mode((largura, altura))
+    py.display.set_caption('Flappy Bird')
+    fps = py.time.Clock()
 
+    all_sprites = py.sprite.Group()
+    wall_sprites = py.sprite.Group()
 
-    all_sprites.draw(tela)
-    all_sprites.update()
-    if pipe.x_img <= -84:
+    for c in range(2):
+        bg = obj.Bg(os.path.join(img_dir, 'bg.png'), c, 1000, 1)
+        all_sprites.add(bg)
+
+    for c in range(3):
         ys = obj.pipeys()
-        pipe.y_img = ys[0]
-        pipe1.y_img = ys[1]
+        x = c*245 + 450
+        pipe = obj.Pipe(os.path.join(img_dir, 'pipe.png'), x, ys[0], 3)
+        pipe1 = obj.Pipe(os.path.join(img_dir, 'pipe.png'), x, ys[1], 3, True)
+        all_sprites.add(pipe)
+        all_sprites.add(pipe1)
+        wall_sprites.add(pipe)
+        wall_sprites.add(pipe1)
 
-    py.display.flip()
+    for c in range(2):
+        piso = obj.Bg(os.path.join(img_dir, 'base.png'), c, 150, 3)
+        all_sprites.add(piso)
+
+    bird = obj.Bird(bird_sheet, 200, 300)
+    all_sprites.add(bird)
+
+    while play:
+        
+        tela.fill(color['Gray'])
+        fps.tick(60)
+        
+        for event in py.event.get():
+            if event.type == QUIT:
+                py.quit()
+                exit()
+            if event.type == KEYDOWN:
+                start = True
+                if event.key == K_SPACE:
+                    bird.jump()
+                if event.key == K_r:
+                    play = False
+
+        colide = py.sprite.spritecollide(bird, wall_sprites, False, py.sprite.collide_mask)
+        
+        all_sprites.draw(tela)
+        if colide:
+            pass
+        elif not start:
+            pass
+        else:
+            all_sprites.update()
+        
+        if pipe.x_img <= -84:
+            ys = obj.pipeys()
+            pipe.y_img = ys[0]
+            pipe1.y_img = ys[1]
+
+        py.display.flip()
